4731af779c7909624937c77dddc2ea0d72f12e9a
Ilya <ilyaphotoshop17@gmail.com>
2020-07-26 23:10:33 +0300

    [add] create manipulating dom with js

--- ExpressionGenerator/index.js
+++ ExpressionGenerator/index.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementsByClassName('expressionGenerator')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementsByClassName('author')
+const twitterBtn = document.getElementsByClassName('twitter')
+const newQuoteBtn = document.getElementsByClassName('new-quote')
+
 
 async function getQuote() {
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json';
@@ -5,12 +11,35 @@ async function getQuote() {
     try {
         const result = await  fetch(proxyUrl + apiUrl)
         const data = await result.json()
-        console.log(data);
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknow'
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+        if (data.quoteText.lenght > 120) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText;
     } catch(e) {
         getQuote()
         console.log("Error: ", e)
     }
 }
 
+function tweetQuote() {
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank')
+}
+
+newQuoteBtn.addEventListner('click', getQuote())
+twitterBtn.addEventListner('click', tweetQuote())
+
 //start
-getQuote()
+
+setInterval(
+    getQuote()
+), 1000
