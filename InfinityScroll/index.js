ada57bbf0cfeb8db47cb6aec27853d95505b9a21
Ilya <ilyaphotoshop17@gmail.com>
2020-07-29 14:24:35 +0300

    [add] js to display data

--- InfinityScroll/index.js
+++ InfinityScroll/index.js
@@ -1,12 +1,44 @@
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+
+let photosArray = [];
+
 const count = 10;
 const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+function displayPhotos() {
+    photosArray.forEach(photo => {
+        const item = document.createElement('a')
+        setAttributes(item, {
+            href: photo.links.html,
+            target:'_blank'
+        })
+
+        const img = document.createElement('img')
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
+
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+    })
+}
+
 async function getPhoto() {
     try {
         const response = await fetch(apiUrl)
-        const data = response.json()
-        console.log(data)
+        photosArray = await response.json()
+        console.log(photosArray)
+        displayPhotos()
     } catch (e) {
         console.log(e)
     }
