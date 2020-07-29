14f2f9badaea8be64f237abe9ea954f1a0a39b33
Ilya <ilyaphotoshop17@gmail.com>
2020-07-29 14:54:30 +0300

    [add] create two new func and added listner at window

--- InfinityScroll/index.js
+++ InfinityScroll/index.js
@@ -1,11 +1,23 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = [];
 
-const count = 10;
+const count = 30;
 const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imageLoaded === totalImages) {
+        ready = true
+        loader.hidden = true
+        console.log('ready: ', ready)
+    }
+}
 
 function setAttributes(element, attributes) {
     for(const key in attributes) {
@@ -14,6 +26,10 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
+    imagesLoaded = 0
+    totalImages = photosArray.length
+    console.log('total images: ',totalImages)
+
     photosArray.forEach(photo => {
         const item = document.createElement('a')
         setAttributes(item, {
@@ -27,6 +43,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         })
+
+        img.addEventListener('load', imageLoaded)
 
         item.appendChild(img)
         imageContainer.appendChild(item)
@@ -43,5 +61,13 @@ async function getPhoto() {
         console.log(e)
     }
 }
+
+window.addEventListener('scroll', () => {
+    console.log('Scroll')
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false
+        getPhoto()
+    }
+})
 
 getPhoto()
