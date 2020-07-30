920f03c16aebe6d21e2861a22a432a77c01fd9b9
Ilya <ilyaphotoshop17@gmail.com>
2020-07-30 11:17:29 +0300

    [add] adding func with js

---
+++ PictureInPicture/index.js
@@ -0,0 +1,22 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+        videoElement.srcObject = mediaStream
+        videoElement.onloadeddata = () => {
+            videoElement.play()
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true
+    await videoElement.requestPictureInPicture()
+    button.disabled = false
+})
+
+selectMediaStream()
