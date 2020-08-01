3269403b3bb88cdae1fd0168f53ef0cb503994a0
Ilya <ilyaphotoshop17@gmail.com>
2020-08-01 11:33:16 +0300

    [add] craete getJokes func

--- JokeTeller/index.js
+++ JokeTeller/index.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('button')
 const audio = document.getElementById('audio')
 
-const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
 const apiKEY = 'e985f868e96c46d9b0789c3855350152'
 
 function tellMe(joke) {
@@ -16,3 +15,23 @@ function tellMe(joke) {
         ssml: false,
     });
 }
+
+async function getJokes() {
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
+    let joke = ''
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+
+        if (data.setup) {
+            joke = `${data.setup} &&  ${data.delivery}`
+        } else {
+            joke = data.joke
+        }
+        console.log(joke)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+button.addEventListener('click', getJokes)
